b5abefab009b33a38ef68e46e03224cbe21fe5c3
microbuilder <contact@microbuilder.eu>
2017-05-13 15:25:15 +0200

    Added Metro52 BSP

---
+++ nRF5_SDK_11.0.0_89a8197/examples/bsp/metro52.h
@@ -0,0 +1,74 @@
+/**************************************************************************/
+/*!
+    @file     metro52.h
+    @author   hathach
+
+    @section LICENSE
+
+    Software License Agreement (BSD License)
+
+    Copyright (c) ${year}, Adafruit Industries (adafruit.com)
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holders nor the
+    names of its contributors may be used to endorse or promote products
+    derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/**************************************************************************/
+
+#ifndef METRO52_H
+#define METRO52_H
+
+#define LEDS_NUMBER    2
+#define LED_1          29
+#define LED_2          22
+#define LED_STATE_ON   1
+
+#define BSP_LED_0      LED_1
+#define BSP_LED_1      LED_2
+
+#define BUTTONS_NUMBER 1
+#define BUTTON_1       18
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+#define BSP_BUTTON_0   BUTTON_1
+
+
+#define BSP_BUTTON_0_MASK (1<<BSP_BUTTON_0)
+#define BSP_BUTTON_1_MASK (1<<BSP_BUTTON_1)
+#define BSP_BUTTON_2_MASK (1<<BSP_BUTTON_2)
+#define BSP_BUTTON_3_MASK (1<<BSP_BUTTON_3)
+
+#define BUTTONS_MASK   0x001E0000
+
+#define RX_PIN_NUMBER  23 // 8 // 12
+#define TX_PIN_NUMBER  8  // 6 // 11
+#define CTS_PIN_NUMBER 0
+#define RTS_PIN_NUMBER 0
+#define HWFC           false
+
+// Low frequency clock source to be used by the SoftDevice
+// Adafruit overwrite by one in main.c
+#define NRF_CLOCK_LFCLKSRC      {.source        = NRF_CLOCK_LF_SRC_XTAL,            \
+                                 .rc_ctiv       = 0,                                \
+                                 .rc_temp_ctiv  = 0,                                \
+                                 .xtal_accuracy = NRF_CLOCK_LF_XTAL_ACCURACY_20_PPM}
+
+#endif // METRO52_H
