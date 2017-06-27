8261001f20b1d7782cef4bb65ea409b67db3f642
hathach <thach@tinyusb.org>
2017-06-27 16:06:21 +0700

    update led on/off to check FRESET/LED2 confliction on metro52

--- nRF5_SDK_11.0.0_89a8197/examples/bsp/boards.h
+++ nRF5_SDK_11.0.0_89a8197/examples/bsp/boards.h
@@ -12,12 +12,16 @@
 #ifndef BOARDS_H
 #define BOARDS_H
 
+#include <stdbool.h>
+#include <stdint.h>
 #include "nrf_gpio.h"
 
 #if defined BOARD_FEATHER52
 #include "feather52.h"
 #elif defined BOARD_METRO52
 #include "metro52.h"
+#else
+#error No boards defined
 #endif
 
 // Make sure we have at least two buttons (DFU + FRESET since DFU+FRST=OTA)
@@ -30,7 +34,41 @@
 #define LED_CONNECTION_PIN    LED_2
 #endif
 
-#define led_on(pin)           nrf_gpio_pin_write(pin, LED_STATE_ON)
-#define led_off(pin)          nrf_gpio_pin_write(pin, 1-LED_STATE_ON)
+#define bit(b) (1UL << (b))
+
+static inline uint32_t bit_set(uint32_t value, uint8_t n)
+{
+  return value | bit(n);
+}
+
+static inline uint32_t bit_clear(uint32_t value, uint8_t n)
+{
+  return value & (~bit(n));
+}
+
+static inline bool bit_test(uint32_t value, uint8_t n)
+{
+  return (value & bit(n)) ? true : false;
+}
+
+static inline void led_control(uint32_t pin, bool state)
+{
+#ifdef BOARD_METRO52
+  // Skip if LED_BLUE is configured as input (possibly wiring to GND)
+  if ( pin == LED_2 && !bit_test(NRF_GPIO->PIN_CNF[pin], GPIO_PIN_CNF_DIR_Pos) ) return;
+#endif
+
+  nrf_gpio_pin_write(pin, state ? LED_STATE_ON : (1-LED_STATE_ON));
+}
+
+static inline void led_on(uint32_t pin)
+{
+  led_control(pin, true);
+}
+
+static inline void led_off(pin)
+{
+  led_control(pin, false);
+}
 
 #endif
