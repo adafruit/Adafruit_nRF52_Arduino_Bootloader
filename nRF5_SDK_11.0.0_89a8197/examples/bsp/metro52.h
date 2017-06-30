ecf29b333cbfdbd15b8c02aff20e55ae74892f97
hathach <thach@tinyusb.org>
2017-06-30 23:02:09 +0700

    clean up

--- nRF5_SDK_11.0.0_89a8197/examples/bsp/metro52.h
+++ nRF5_SDK_11.0.0_89a8197/examples/bsp/metro52.h
@@ -7,7 +7,7 @@
 
     Software License Agreement (BSD License)
 
-    Copyright (c) ${year}, Adafruit Industries (adafruit.com)
+    Copyright (c) 2017, Adafruit Industries (adafruit.com)
     All rights reserved.
 
     Redistribution and use in source and binary forms, with or without
